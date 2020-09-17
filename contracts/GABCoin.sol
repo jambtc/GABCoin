07946169446f897ebd37eaf57c1e02a7abb4bb8c
jambtc <30697838+jambtc@users.noreply.github.com>
2020-09-17 13:41:03 +0200

    Update and rename TTSToken.sol to GABCoin.sol
    Update and rename TTSToken.sol to GABCoin.sol

--- contracts/GABCoin.sol
+++ contracts/GABCoin.sol
@@ -4,12 +4,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
-/// @author Antonio Della Porta
+/// @author Sergio Casizzone
 
-contract TTSToken is ERC20, ERC20Detailed, ERC20Mintable {
-	uint private INITIAL_SUPPLY = 1e3;
+contract GABCoin is ERC20, ERC20Detailed, ERC20Mintable {
+	uint private INITIAL_SUPPLY = 1e9;
 	
-	constructor() ERC20Mintable() ERC20Detailed("Token Temporaneo di Sconto", "TTS", 2) ERC20() public {
+	constructor() ERC20Mintable() ERC20Detailed("GabCoin Token", "GAB", 2) ERC20() public {
 	    _mint(msg.sender, INITIAL_SUPPLY);
 	}
 }
